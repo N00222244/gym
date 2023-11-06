fb626a6c16f17c6fa1bbd591ab6a86d64622a1a5
N00222244 <N00222244@iadt.ie>
2023-11-06 16:04:21 +0000

    initial commit + model + controller

---
+++ app/Models/Group.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Group extends Model
+{
+    use HasFactory;
+
+
+    protected $fillable ={
+        'group_name'
+        'group_time'
+        'group_date'
+        'group_type'
+        'group_image'
+    };
+}
