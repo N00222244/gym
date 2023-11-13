e65ea326ab1d49300347066e6a78ce1403c5c95a
N00222244 <N00222244@iadt.ie>
2023-11-13 14:43:41 +0000

    created two users in userseeder+ added ad/us roles

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -12,6 +12,22 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $role_admin = Role::where('name', 'admin')->first();
+
+        $role_user = Role::where('name', 'user')->first();
+
+        $admin = new User();
+        $admin->name = 'Adam Doyle';
+        $admin->email = 'adam@larafest.ie';
+        $admin->password = Hash::make('password');
+        $admin->save();
+
+        $admin->roles()->attach($role_admin);
+
+        $user = new User();
+        $user->name = 'Joe Jones';
+        $user->password = Hash::make('password');
+        $user->save();
+        $user->roles()attach($role_user);
     }
 }
